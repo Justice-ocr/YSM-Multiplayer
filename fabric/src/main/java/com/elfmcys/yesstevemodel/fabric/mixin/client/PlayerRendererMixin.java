96070bdc983e4f581bb610ee28997ab5b92cd9e4
Justice-ocr <yueyutong311@gmail.com>
2026-06-02 13:25:54 +0800

    Bind player render states to players

--- fabric/src/main/java/com/elfmcys/yesstevemodel/fabric/mixin/client/PlayerRendererMixin.java
+++ fabric/src/main/java/com/elfmcys/yesstevemodel/fabric/mixin/client/PlayerRendererMixin.java
@@ -1,11 +1,11 @@
 package com.elfmcys.yesstevemodel.fabric.mixin.client;
 
 import com.elfmcys.yesstevemodel.client.event.ReplacePlayerRenderEvent;
-import com.elfmcys.yesstevemodel.fabric.accessor.EntityRenderDispatcherAccessor;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
@@ -13,6 +13,15 @@ import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.client.renderer.entity.state.PlayerRenderState;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+import java.util.WeakHashMap;
 
 /**
  * @author IzumiiKonata
@@ -21,16 +30,22 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(PlayerRenderer.class)
 public abstract class PlayerRendererMixin extends LivingEntityRenderer<AbstractClientPlayer, PlayerRenderState, PlayerModel> {
 
+    @Unique
+    private final Map<PlayerRenderState, Player> ysm$playersByRenderState = Collections.synchronizedMap(new WeakHashMap<>());
+
     public PlayerRendererMixin(EntityRendererProvider.Context context, PlayerModel entityModel, float f) {
         super(context, entityModel, f);
     }
 
+    @Inject(method = "extractRenderState(Lnet/minecraft/client/player/AbstractClientPlayer;Lnet/minecraft/client/renderer/entity/state/PlayerRenderState;F)V", at = @At("RETURN"))
+    private void ysm$rememberRenderStatePlayer(AbstractClientPlayer player, PlayerRenderState renderState, float partialTick, CallbackInfo ci) {
+        ysm$playersByRenderState.put(renderState, player);
+    }
+
     @Override
     public void render(PlayerRenderState livingEntityRenderState, PoseStack poseStack, MultiBufferSource multiBufferSource, int packedLight) {
-        net.minecraft.world.entity.Entity lastEntity =
-            ((EntityRenderDispatcherAccessor) Minecraft.getInstance().getEntityRenderDispatcher()).ysm$getLastRenderingEntity();
-
-        if (lastEntity instanceof Player player && ReplacePlayerRenderEvent.onRenderPlayerPre(
+        Player player = ysm$getPlayer(livingEntityRenderState);
+        if (player != null && ReplacePlayerRenderEvent.onRenderPlayerPre(
                 player, livingEntityRenderState,
                 Minecraft.getInstance().getDeltaTracker().getGameTimeDeltaPartialTick(true),
                 poseStack, multiBufferSource, packedLight)) {
@@ -38,5 +53,21 @@ public abstract class PlayerRendererMixin extends LivingEntityRenderer<AbstractC
         }
 
         super.render(livingEntityRenderState, poseStack, multiBufferSource, packedLight);
+    }
+
+    @Unique
+    private Player ysm$getPlayer(PlayerRenderState renderState) {
+        Player player = ysm$playersByRenderState.get(renderState);
+        if (player != null) {
+            return player;
+        }
+
+        LocalPlayer localPlayer = Minecraft.getInstance().player;
+        if (localPlayer != null
+                && renderState.id == localPlayer.getId()
+                && Objects.equals(renderState.name, localPlayer.getGameProfile().getName())) {
+            return localPlayer;
+        }
+        return null;
     }
 }
